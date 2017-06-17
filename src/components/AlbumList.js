fee6172f02c71f55cad1918b6bf625a6c75365f0
Adam Tse <mr.adam.tse@gmail.com>
2017-06-17 15:07:10 -0400

    create an card component

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,21 +1,33 @@
 import React, { Component } from 'react';
 import { View, Text} from 'react-native';
 import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 
 
 class AlbumList extends Component {
+  state = { albums: []}
+  
+  renderAlbums(){
+    return this.state.albums.map(album => {
+      return <AlbumDetail album={album} key={album.title} />
+    });
+  }
+  
   componentWillMount() {
     axios.get('http://rallycoding.herokuapp.com/api/music_albums')
          .then(response => {
-           console.log(response);
+           this.setState({
+             albums: response.data
+           })
          })
     console.log('HI');
     console.log('how are you???');
   }
   render(){
+    console.log(this.state);
     return (
       <View>
-        <Text> Album List </Text>
+        {this.renderAlbums()}
       </View>
     )
   }
---
+++ src/components/Card.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const Card = (props) => {
+  
+  return (
+    <View>
+    </View>
+  )
+}
+
+const styles = {
+  containerStyle: {
+    borderWidth: 1,
+    borderRadius: 2,
+    borderColor: '#ddd',
+    borderBottomWidth: 0
+  }
+}
+export default Card;
